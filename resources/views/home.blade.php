7319b43c509a45a7889a490c75cbacb6ef4766f3
NashrulMessi <nashrulhd@gmail.com>
2025-05-23 14:57:20 +0700

    loli

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-</div>
+ @extends('layouts.app')
+
+@section('title', 'kontak')
+
+    
+@section('content')   
+ <section class="">
+        <div class="">
+            <h2>Siap Siaga, Cepat Tanggap, Melindungi Masyarakat</h2>
+            <p>Dinas Pemadam Kebakaran siap 24 jam setiap hari untuk melindungi keselamatan dan properti masyarakat dari bahaya kebakaran dan bencana lainnya.</p>
+            <a href="#kontak" class="cta-button">Hubungi Kami</a>
+        </div>
+    </section>
+@endsection
