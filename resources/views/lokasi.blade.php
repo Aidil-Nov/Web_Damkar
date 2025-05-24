e6674534c174c893e104ee88419476bc5ea02b39
NashrulMessi <nashrulhd@gmail.com>
2025-05-24 17:45:58 +0700

    komit

--- resources/views/lokasi.blade.php
+++ resources/views/lokasi.blade.php
@@ -13,21 +13,31 @@
                 </h2>
             </div>
 
-            <div class="grid">
-                <!-- Pos Pusat -->
-                <div class=" rounded-md shadow-md overflow-hidden">
-                    <div class="h-48 bg-cover bg-center" style="background-image: url('/api/placeholder/400/200');"></div>
-                    <div class="p-6 space-y-2">
-                        <h3 class="text-xl font-semibold text-gray-800">Posko Damkar Kurnia Kota Baru</h3>
-                        <p class="text-gray-600">Pos utama dengan peralatan lengkap dan personel siaga 24 jam.</p>
-                        <div class="text-sm text-gray-700 space-y-1">
+        <div class="grid">
+            <!-- Pos Pusat -->
+            <div class="w-full">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.815889811015!2d109.30541527477625!3d-0.06324289993613172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e1d595d4aa6b035%3A0xa745174c88c2d74f!2sJl.%20Kurnia%2C%20Kota%20Baru%2C%20Kec.%20Pontianak%20Sel.%2C%20Kota%20Pontianak%2C%20Kalimantan%20Barat%2078115!5e0!3m2!1sid!2sid!4v1748078560215!5m2!1sid!2sid"
+                    class="w-full h-64" style="border:0;" allowfullscreen="true" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+                <div class="rounded-md shadow-md overflow-hidden">
+                    <div class="h-48 bg-cover bg-center">
+                        <div class="p-6 space-y-2">
+                            <h3 class="text-xl font-semibold">Pos Pemadam Pusat</h3>
+                            <p class="">Pos utama dengan peralatan lengkap dan personel siaga 24 jam.</p>
+                            <div class="text-sm  space-y-1">
                             <p><span class="mr-2">📍</span> Jl. Prof. M. Yamin - Jl. Kurnia</p>
                             <p><span class="mr-2">📞</span>(+62) 81254475004</p>
                             <p><span class="mr-2">⏰</span>Beroperasi 24 Jam</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+        </div>
+    </div>
         </div>
     </section>
 
