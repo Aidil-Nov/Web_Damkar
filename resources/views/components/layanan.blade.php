df045cc06c7694bd74ba011cd9e1727a566a43b4
NashrulMessi <nashrulhd@gmail.com>
2025-05-25 17:13:37 +0700

    pusyyy

--- resources/views/components/layanan.blade.php
+++ resources/views/components/layanan.blade.php
@@ -12,26 +12,28 @@
         <!-- Grid Layanan -->
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <!-- Kartu Layanan 1 -->
-            <div class="bg-white shadow-md hover:shadow-xl transition rounded-lg overflow-hidden">
+            <a href="{{ route('Layanan') }}" ><div class="bg-white shadow-md hover:shadow-xl transition rounded-lg overflow-hidden">
                 <div class="h-48 bg-cover bg-center" style="background-image: url('/assets/images/1.jpeg');"></div>
                 <div class="p-6">
                     <h3 class="text-xl font-semibold text-red-700 mb-2">Pemadaman Kebakaran</h3>
-                    <p class="text-gray-700">Tim kami terlatih untuk menangani berbagai jenis kebakaran dengan peralatan
-                        modern dan respons cepat untuk mem inimalisir kerugian.</p>
+                    <p class="text-gray-700">Tim kami terlatih untuk menangani berbagai jenis kebakaran dengan respons yang 
+                        cepat untuk meminimalisir kerugian.</p>
                 </div>
-            </div>  
+            </div>  </a>
+            
 
             <!-- Kartu Layanan 2 -->
-            <div class="bg-white shadow-md hover:shadow-xl transition rounded-lg overflow-hidden">
+               <a href="{{ route('Layanan') }}" > <div class="bg-white shadow-md hover:shadow-xl transition rounded-lg overflow-hidden">
                 <div class="h-48 bg-cover bg-center" style="background-image: url('/assets/images/anggota.jpg');"></div>
                 <div class="p-6">
                     <h3 class="text-xl font-semibold text-red-700 mb-2">Penyelamatan dan Evakuasi</h3>
                     <p class="text-gray-700">Layanan penyelamatan dan evakuasi untuk situasi darurat seperti bencana
                         alam, kecelakaan, atau kondisi berbahaya lainnya.</p>
                 </div>
-            </div>
+            </div> </a>
 
             <!-- Kartu Layanan 3 -->
+             <a href="{{ route('Layanan') }}" > 
             <div class="bg-white shadow-md hover:shadow-xl transition rounded-lg overflow-hidden">
                 <div class="h-48 bg-cover bg-center" style="background-image: url('/assets/images/pembersihan saluran air 3.jpg');"></div>
                 <div class="p-6">
@@ -39,7 +41,7 @@
                     <p class="text-gray-700">Program-program edukasi dan pelatihan untuk masyarakat tentang cara
                         mencegah kebakaran dan penanganan kondisi darurat.</p>
                 </div>
-            </div>
+            </div></a>
 
         </div>
     </div>
