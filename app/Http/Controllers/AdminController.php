e448656870114e54ef2b888d627c97cc2914c9ba
Aidil-Nov <lomantp537.com>
2025-07-20 23:37:25 +0700

    admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactMessage;
+use App\Models\News;
+use App\Models\Gallery;
+use App\Models\Visitor;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function contactMessages()
+    {
+        $messages = ContactMessage::latest()->get();
+        return view('admin.contact_messages', compact('messages'));
+    }
+
+    public function newsIndex()
+    {
+        $news = News::latest()->get();
+        return view('admin.news.index', compact('news'));
+    }
+
+    public function galleryIndex()
+    {
+        $galleries = Gallery::latest()->get();
+        return view('admin.gallery.index', compact('galleries'));
+    }
+
+    public function visitorIndex()
+    {
+        $visitors = Visitor::latest()->get();
+        return view('admin.visitors.index', compact('visitors'));
+    }
+}
